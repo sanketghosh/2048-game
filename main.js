a650be0c105ef6a11f329e749aa537aee17305be
sanket <code.sanket13@gmail.com>
2024-05-09 00:00:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+openModalBtn.addEventListener("click", openModal);
